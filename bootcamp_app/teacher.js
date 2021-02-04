b71705fc149ed3da40367e8834467053a470366e
Hsing-I Wang <hsing.sophie@gmail.com>
2021-02-04 00:38:05 +0000

    avoid sql injection

--- bootcamp_app/teacher.js
+++ bootcamp_app/teacher.js
@@ -8,14 +8,10 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2] || "JUL02";
 
-pool.query(`SELECT DISTINCT teachers.name AS "teacher", cohorts.name AS "cohort"
-FROM teachers 
-JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
-JOIN students ON assistance_requests.student_id = students.id
-JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name =  '${process.argv[2] || 'JUL02'}' 
-ORDER BY teacher`)
+pool.query(`SELECT DISTINCT teachers.name AS "teacher", cohorts.name AS "cohort" FROM teachers JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id 
+JOIN students ON assistance_requests.student_id = students.id JOIN cohorts ON students.cohort_id = cohorts.id WHERE cohorts.name = $1 ORDER BY teacher`, [cohortName])
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
